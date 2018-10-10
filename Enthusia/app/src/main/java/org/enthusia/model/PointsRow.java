0a6137bc40c53188c751edf0dc496994890ba014
merchantaliabbas@gmail.com <merchantaliabbas@gmail.com>
2018-10-10 22:41:10 +0530

    Added Points Table

---
+++ Enthusia/app/src/main/java/org/enthusia/model/PointsRow.java
@@ -0,0 +1,44 @@
+package org.enthusia.model;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.constraint.ConstraintLayout;
+import android.view.LayoutInflater;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+import org.enthusia.R;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+public class PointsRow extends ConstraintLayout {
+    TextView sr_no, team_name, p, w, l, pf, pa, pts;
+    JSONObject data;
+    public PointsRow(Context context) {
+        super(context);
+    }
+    public PointsRow(Context context, JSONObject data) {
+        super(context);
+        this.data = data;
+        LayoutInflater.from(context).inflate(R.layout.match_card,this,true);
+        sr_no = findViewById(R.id.sr_no);
+        team_name = findViewById(R.id.team_name);
+        p = findViewById(R.id.p);
+        w = findViewById(R.id.w);
+        l = findViewById(R.id.l);
+        pf = findViewById(R.id.pf);
+        pa = findViewById(R.id.pa);
+        pts = findViewById(R.id.pts);
+        inflate_data();
+    }
+    public JSONObject getData(){
+        return data;
+    }
+
+    public void inflate_data() {
+        
+    }
+
+}
